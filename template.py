b6a63f7bb42e7e974fc9f099866182aea4c5fe0c
Felix Plymouth <fplymouth@gmail.com>
2023-11-30 20:36:04 +0100

    Setup day 1

---
+++ template.py
@@ -0,0 +1,11 @@
+import itertools
+import operator
+from functools import reduce
+
+text = open("day1.txt").read()
+lines = text.splitlines()
+
+print("Part 1: ", "incomplete")
+
+
+print("Part 2: ", "incomplete")
